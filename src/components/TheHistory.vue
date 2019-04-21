a4f4f54a055868485dee40c31221126dd2051d46
Pavel Bazin <pvlbzn@gmail.com>
2019-04-21 18:51:40 +0200

    history component

--- src/components/TheHistory.vue
+++ src/components/TheHistory.vue
@@ -2,8 +2,11 @@
 <table v-if="records.length !== 0" class="ui celled table">
   <thead>
     <tr>
-      <th>Generation</th>
-      <th>Score</th>
+      <th class="table-head table__subheader">Generation</th>
+      <th class="table-head table__subheader">Avg Score</th>
+      <th class="table-head table__subheader">Avg Duration</th>
+      <th class="table-head table__subheader">Avg Steps</th>
+      <th class="table-head table__subheader">Avg Rewards</th>
     </tr>
   </thead>
   <tbody>
@@ -11,8 +14,11 @@
       v-for="record in records" 
       :key="record.generation"
     >
-      <td data-label="Generation">{{ record.generation }}</td>
-      <td data-label="Score">{{ record.score }}</td>
+      <td data-label="Generation" class="table__body">{{ record.generation }}</td>
+      <td data-label="Score" class="table__body">{{ record.score }}</td>
+      <td data-label="Duration" class="table__body">{{ Math.floor(record.duration) }}</td>
+      <td data-label="Steps" class="table__body">{{ Math.floor(record.steps) }}</td>
+      <td data-label="Rewards" class="table__body">{{ record.rewards.toFixed(2) }}</td>
     </tr>
   </tbody>
 </table>
@@ -29,6 +35,31 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+.table-head {
+  background: #F4F4F4 !important;
+  border: none !important;
+}
 
+.ui.celled.table {
+  box-shadow: none !important;
+  border: none;
+}
+
+.table__subheader {
+  color: #676767 !important;
+  font-weight: bold !important;
+  font-size: 16px !important;
+}
+
+td.table__body {
+  border-color: #ffffff !important;
+  font-family: monospace;
+  font-size: 1em !important;
+  font-weight: 400 !important;
+  background: #F4F4F4 !important;
+  margin-left: 1em !important;
+  padding: 4px !important;
+  padding-left: 1em !important;
+}
 </style>
